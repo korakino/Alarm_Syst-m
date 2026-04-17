ec470669c4cc6c84b70fb70946c1fb652c5a0ade
Bastien FAUGIER <133578470+korakino@users.noreply.github.com>
2026-04-17 07:24:52 +0000

    creation of main file

---
+++ main.ino
@@ -0,0 +1,15 @@
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3,OUTPUT); //LED verte
+  pinMode(4,OUTPUT); //LED jaune
+  pinMode(5,OUTPUT); // LED rouge
+  pinMode(7,OUTPUT); // haut parleur
+  pinMode(2,INPUT_PULLUP); // BP
+}
+
+void loop (){
+    
+}
